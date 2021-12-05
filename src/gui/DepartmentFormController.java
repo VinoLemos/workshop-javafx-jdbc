b5c82e2c5824e698231bfba6d2f50fe20ebd88bf
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2021-12-05 16:46:22 -0300

    ValidationException

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,9 +20,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
+
+	// Botões, Labels e TextFields
+		@FXML
+		private TextField txtId;
+		@FXML
+		private TextField txtName;
+		@FXML
+		private Label labelErrorName;
+		@FXML
+		private Button btSave;
+		@FXML
+		private Button btCancel;
 
 	// Dependência de Department e DepartmentService
 	private Department entity;
@@ -38,18 +53,6 @@ public class DepartmentFormController implements Initializable {
 		dataChangeListeners.add(listener);
 	}
 
-// Botões, Labels e TextFields
-	@FXML
-	private TextField txtId;
-	@FXML
-	private TextField txtName;
-	@FXML
-	private Label labelErrorName;
-	@FXML
-	private Button btSave;
-	@FXML
-	private Button btCancel;
-
 // Ações a serem executadas	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
@@ -65,9 +68,17 @@ public class DepartmentFormController implements Initializable {
 		notifyDataChangeListeners();
 		Utils.currentStage(event).close();
 		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());	
+			}
 		catch (DbException e ) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
+	}
+	
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 
 	private void notifyDataChangeListeners() {
@@ -75,17 +86,27 @@ public class DepartmentFormController implements Initializable {
 			listener.onDataChanged();
 		}
 }
+	
+	
 	// Insere os dados do DepartmentForm no objeto
 	private Department getFormData() {
 		Department obj = new Department();
+		
+		ValidationException exception = new ValidationException("Validation Error!");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty!");
+		}
 		obj.setName(txtName.getText());
+		
+		if ( exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		return obj;
 	}
-	@FXML
-	public void onBtCancelAction(ActionEvent event) {
-		Utils.currentStage(event).close();
-	}
+
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
@@ -99,5 +120,13 @@ public class DepartmentFormController implements Initializable {
 		}
 			txtId.setText(String.valueOf(entity.getId()));
 			txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 }
